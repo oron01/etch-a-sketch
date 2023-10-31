9b34356d3c746ac964aaf1011686a8704c1b48c4
oron01 <136211946+oron01@users.noreply.github.com>
2023-10-31 21:52:29 +0200

    complete original code requirements

--- code.js
+++ code.js
@@ -3,26 +3,56 @@ let boardDefinition = 16
 let squareContainer = document.querySelector('#squareContainer')
 let setSquaresButton = document.querySelector('#setSquaresButton')
 
+let invertButton = document.querySelector("#invertColorButton")
+let randomButton = document.querySelector("#randomColorButton")
+let darkenButton = document.querySelector("#darkenButton")
+
+let currentFunctionSwitch = "invertColorButton"
+
 let setInvertedColor = (square) => {
-    if (square.target.className.includes(" clicked")) {
-    square.target.className = square.target.className.replace(" clicked", " unclicked")}
-    else if (square.target.className.includes("unclicked")) {square.target.className = square.target.className.replace("unclicked", "clicked")}
-    else (square.target.className = "square clicked")}
+    if (square.target.style.backgroundColor == "" || square.target.style.backgroundColor == "rgb(255,255,255)") {
+        square.target.style.backgroundColor = "rgb(0,0,0)" }
+    else {
+        square.target.style.backgroundColor = ""}
+  }
 
 let getDarkerColor = (color) => {
     color = color.slice(4,-1)
     color = color.split(",")
-    newColor = color.map((color) => {color + 25})
-    alert(newColor)
+    let newColor = color.map((a) => +a - 25)
+    newColor = `RGB(${newColor[0]},${newColor[1]},${newColor[2]})`
+    return newColor
+}
+
+let switchGameFunction = (square) => {
+    let functionSwitch
+    if (square == null) { functionSwitch = currentFunctionSwitch}
+    else {functionSwitch = square.target.id
+    // alert(currentFunctionSwitch)
+    }
+    currentFunctionSwitch = functionSwitch
+    switch (functionSwitch) {
+        case "invertColorButton":
+            addSquareEventListener(setInvertedColor,'click',"add")
+            addSquareEventListener(setRandomColor,'click',"remove")
+            addSquareEventListener(setDarkerColor,'click',"remove")
+            break;
+        case "randomColorButton":
+            addSquareEventListener(setInvertedColor,'click',"remove")
+            addSquareEventListener(setRandomColor,'click',"add")
+            addSquareEventListener(setDarkerColor,'click',"remove")
+            break;
+        case "darkenButton":
+            addSquareEventListener(setInvertedColor,'click',"remove")
+            addSquareEventListener(setRandomColor,'click',"remove")
+            addSquareEventListener(setDarkerColor,'click',"add")
+            break;
+    } 
 }
 
 let setDarkerColor = (square) => {
-    if (square.target.className.includes(" clicked")) {
-        square.target.className = square.target.className.replace(" clicked", " unclicked")}
-    else if (square.target.className.includes("unclicked")) 
-    {square.target.className = square.target.className.replace("unclicked", "")}
+square.target.style.backgroundColor = getDarkerColor(square.target.style.backgroundColor)
 }
-
 
 let getRandomColor = () => {
     let threeRGBCount = 0
@@ -45,12 +75,19 @@ let setRandomColor = (square) => {
     square.target.style.backgroundColor = getRandomColor()    
 }
 
-let addSquareEventListener = (anyFunction,eventType) => {
+let addSquareEventListener = (anyFunction,eventType,addOrRemove) => {
     let allSquares = document.querySelectorAll(".square")
     console.log(allSquares)
+    if (addOrRemove == "add") {
     allSquares.forEach((square) => {
         square.addEventListener(eventType,anyFunction)
-    })
+    })}
+    else if (addOrRemove == "remove") {
+        allSquares.forEach((square) => {
+            square.removeEventListener(eventType,anyFunction)
+        })
+    }
+    
 }
 
 
@@ -90,8 +127,8 @@ let setEtch = () => {
 
 setEtch()
 
-// addSquareEventListener(setInvertedColor,'click')
-addSquareEventListener(setRandomColor,'click')
+addSquareEventListener(setInvertedColor,'click',"add")
+// addSquareEventListener(setRandomColor,'click')
 
 let resetEtch = () => {
     boardDefinition = +(prompt("how many squares?"))
@@ -99,8 +136,14 @@ let resetEtch = () => {
     {alert(`Illegal Input, setting to 1`)
 boardDefinition = 1}
     setEtch()
-    addSquareEventListener(setInvertedColor,'click')
+    switchGameFunction()
+    // addSquareEventListener(setInvertedColor,'click')
 }
 
 setSquaresButton.addEventListener('click',resetEtch)
 
+invertButton.addEventListener('click',switchGameFunction)
+randomButton.addEventListener('click',switchGameFunction)
+darkenButton.addEventListener('click',switchGameFunction)
+
+//consider adding a spray mode where you can just hold down or mouse enter
