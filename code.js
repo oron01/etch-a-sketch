86f634840f72fd2e6536101790ea9040cd774a8a
oron01 <136211946+oron01@users.noreply.github.com>
2023-10-31 07:35:04 +0200

    create hoverable pad

--- code.js
+++ code.js
@@ -3,22 +3,38 @@ let boardDefinition = 16
 let squareContainer = document.querySelector('#squareContainer')
 let setSquaresButton = document.querySelector('#setSquaresButton')
 
-let addSquare = (i) => {
-    i = document.createElement('div')
-    i.className = 'square'
-    squareContainer.appendChild(i)
+let causeHoverEffect = (square) => (
+    square.className = "red hover"
+) 
+
+let removeHoverEffect = () => {
+    square.backgroundColor = "red"
+}
+
+let addSquare = (square,container) => {
+    square = document.createElement('div')
+    square.className = 'square'
+    container.appendChild(square)
+}
+
+let addMiniContainer = (container) => {
+    container = document.createElement('div')
+    container.className = 'miniContainer'
+    squareContainer.appendChild(container)
+    return(container)
 }
 
 let removeAllSquares = () => {
-    let allSquares = document.querySelectorAll('.square')
-    allSquares.forEach((square) => {squareContainer.removeChild(square)})
+    let allMiniContainers = document.querySelectorAll('.miniContainer')
+    allMiniContainers.forEach((miniContainer) => {squareContainer.removeChild(miniContainer)})
 }
 
 let setEtch = () => {
     removeAllSquares()
-    for (let i = 0; i < boardDefinition; i++) {
-        for (let i = 0; i < boardDefinition; i++) {
-            addSquare(i)
+    for (let containerCount = 0; containerCount < boardDefinition; containerCount++) {
+        let container = addMiniContainer(containerCount)
+        for (let square = 0; square < boardDefinition; square++) {
+            addSquare(square,container)
 
         }
 
@@ -28,7 +44,10 @@ let setEtch = () => {
 setEtch()
 
 let resetEtch = () => {
-    boardDefinition = prompt("how many squares?")
+    boardDefinition = +(prompt("how many squares?"))
+    if (boardDefinition < 1 || isNaN(boardDefinition) || boardDefinition > 100)
+    {alert(`Illegal Input, setting to 1`)
+boardDefinition = 1}
     setEtch()
 }
 
